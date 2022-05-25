2d1391dd4d76c57b5d341d6732c4851f8c87a083
PKosorukikh <pkosorukih@gmail.com>
2022-05-25 19:46:19 +0300

    create authentifications correct

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
                   path('snippets/add', views.add_snippet_page, name="snippets-add"),
                   path('snippets/list', views.snippets_page, name="snippets-list"),
                   path('snippet/<int:id>', views.snippet_detail, name="snippet-detail"),
-                  path('url/', views.login_page, name='login'),
+                  path('url', views.login_page, name='login'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
